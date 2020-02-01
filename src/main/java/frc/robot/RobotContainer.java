e244e310592115be482cb608d8db8891d308753d
Bryn Dole <bryn.dole@gmail.com>
2020-02-01 14:50:45 -0800

    add simpe auton, drive 1m forward

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,30 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project. */
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
+import static frc.robot.Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared;
+import static frc.robot.Constants.AutoConstants.kMaxSpeedMetersPerSecond;
+import static frc.robot.Constants.AutoConstants.kRamseteB;
+import static frc.robot.Constants.AutoConstants.kRamseteZeta;
+import static frc.robot.Constants.driveConstants.kDriveKinematics;
+import java.util.List;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.driveConstants;
 import frc.robot.commands.driveCommand;
@@ -26,7 +42,7 @@ import frc.robot.subsystems.controlPanelSubsystem;
 
 public class RobotContainer {
   // Subsystems
-  private final driveSubsystem m_driveSubsystem = new driveSubsystem();
+  private final driveSubsystem m_drive = new driveSubsystem();
   private final turretSubsystem m_turretSubsystem = new turretSubsystem();
   private final shooterSubsystem m_shooter = new shooterSubsystem();
   private final indexerSubsystem m_indexer = new indexerSubsystem();
@@ -45,7 +61,7 @@ public class RobotContainer {
   public static XboxController m_operatorController = new XboxController(driveConstants.operatorController);
   public RobotContainer() {
     configureButtonBindings();
-    m_driveSubsystem.setDefaultCommand(new driveCommand(m_driveSubsystem));
+    m_drive.setDefaultCommand(new driveCommand(m_drive));
     m_turretSubsystem.setDefaultCommand(new limelightTurretVisionCommand(m_turretSubsystem));
   }
 
@@ -66,11 +82,62 @@ public class RobotContainer {
     xbutton.whenPressed(() -> m_shooter.setShooterRPM(2500));
     ybutton.whenPressed(() -> m_shooter.setShooterRPM(3000));
   }
-  /*
-  public Command getAutonomousCommand() {
-    An ExampleCommand will run in autonomous
-    limelightVisionCommand m_autoCommand = new limelightVisionCommand();
-    return m_autoCommand;
+  
+  public Command getNoAutonomousCommand() {
+    return new RunCommand(() -> m_drive.tankDriveVolts(0, 0));
   }
-  */
+  
+  public Command getAutonomousCommand() {
+
+    // Drive forward 1 meter
+    RamseteCommand ramseteCommand = createTrajectoryCommand(
+      new Pose2d(0, 0, new Rotation2d(0)),
+      List.of(),
+      new Pose2d(1.0, 0.0, new Rotation2d(0)));
+
+    return ramseteCommand.andThen(() -> m_drive.tankDriveVolts(0, 0));
+  }
+
+  public RamseteCommand createTrajectoryCommand(Pose2d startPose, List<Translation2d> translationList, Pose2d endPose) {
+    DifferentialDriveVoltageConstraint autoVoltageConstraint;
+    TrajectoryConfig config;
+  
+    // Create a voltage constraint to ensure we don't accelerate too fast
+    autoVoltageConstraint = new DifferentialDriveVoltageConstraint(m_drive.getFeedforward(), kDriveKinematics, 6);
+
+    // Create config for trajectory
+    config = new TrajectoryConfig(kMaxSpeedMetersPerSecond, kMaxAccelerationMetersPerSecondSquared)
+        // Add kinematics to ensure max speed is actually obeyed
+        .setKinematics(kDriveKinematics)
+        // Apply the voltage constraint
+        .addConstraint(autoVoltageConstraint);
+
+    var initalTime = System.nanoTime();
+
+    // trajectory to follow. All units in meters.
+   var trajectory = TrajectoryGenerator.generateTrajectory(
+        startPose,
+        translationList,
+        endPose,
+        config);
+
+    RamseteCommand ramseteCommand =
+        new RamseteCommand(trajectory, 
+            m_drive::getPose,
+            new RamseteController(kRamseteB, kRamseteZeta),
+            m_drive.getFeedforward(),
+            kDriveKinematics,
+            m_drive::getWheelSpeeds,
+            m_drive.getLeftPidController(),
+            m_drive.getRightPidController(),
+            m_drive::tankDriveVolts,
+            m_drive);
+
+    var dt = (System.nanoTime() - initalTime) / 1E6;
+    System.out.println("RamseteCommand generation time: " + dt + "ms");
+
+    // Run path following command, then stop at the end.
+    return ramseteCommand;
+  }
+  
 }
