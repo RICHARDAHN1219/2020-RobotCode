15d059d862dd7ecfc085f36d17133174c35842de
jestsh <jestsh@users.noreply.github.com>
2020-02-07 17:35:17 -0800

    deleted unnecessary information

--- src/main/java/frc/robot/commands/controlPanelCommand.java
+++ src/main/java/frc/robot/commands/controlPanelCommand.java
@@ -9,9 +9,8 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.controlPanelConstants;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import frc.robot.subsystems.controlPanelSubsystem;
 
 
 public class controlPanelCommand extends CommandBase {
@@ -19,42 +18,16 @@ public class controlPanelCommand extends CommandBase {
    * Creates a new controlPanelCommand.
    */
   private WPI_TalonSRX colorWheelMotor = new WPI_TalonSRX(controlPanelConstants.motor);
-
-  public controlPanelCommand() {
-    colorWheelMotor.configFactoryDefault();
-    colorWheelMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, controlPanelConstants.PIDLoopIdx, controlPanelConstants.timeoutMs);
-    colorWheelMotor.setSensorPhase(controlPanelConstants.sensorPhase);
-    colorWheelMotor.setInverted(controlPanelConstants.motorInvert);
-    colorWheelMotor.configNominalOutputForward(0, controlPanelConstants.timeoutMs);
-    colorWheelMotor.configNominalOutputReverse(0, controlPanelConstants.timeoutMs);
-    colorWheelMotor.configPeakOutputForward(.2, controlPanelConstants.timeoutMs);
-    colorWheelMotor.configPeakOutputReverse(-.2, controlPanelConstants.timeoutMs);
-    colorWheelMotor.configAllowableClosedloopError(0, controlPanelConstants.PIDLoopIdx, controlPanelConstants.timeoutMs);
-    colorWheelMotor.config_kF(controlPanelConstants.PIDLoopIdx, controlPanelConstants.gains.kF, controlPanelConstants.timeoutMs);
-    colorWheelMotor.config_kP(controlPanelConstants.PIDLoopIdx, controlPanelConstants.gains.kP, controlPanelConstants.timeoutMs);
-    colorWheelMotor.config_kI(controlPanelConstants.PIDLoopIdx, controlPanelConstants.gains.kI, controlPanelConstants.timeoutMs);
-    colorWheelMotor.config_kD(controlPanelConstants.PIDLoopIdx, controlPanelConstants.gains.kD, controlPanelConstants.timeoutMs);
-    int absolutePosition = colorWheelMotor.getSelectedSensorPosition();
-    absolutePosition &= 0xFFF;
-
-    if (controlPanelConstants.sensorPhase) {
-      absolutePosition *= -1;
-    }
-
-    if (controlPanelConstants.motorInvert) {
-      absolutePosition *= -1;
-    }
-
-    colorWheelMotor.setSelectedSensorPosition(0, controlPanelConstants.PIDLoopIdx, controlPanelConstants.timeoutMs);
-  }
-
-  public void setSpeed(double speed) {
-    colorWheelMotor.set(ControlMode.Velocity, speed);
-  }
-
+  
   public void setPosition(double position) {
-    colorWheelMotor.set(ControlMode.Position, position);
+  
   }
+
+  public controlPanelCommand(controlPanelSubsystem colorWheelSubsystem) {
+    
+
+  }
+ 
     // Use addRequirements() here to declare subsystem dependencies.
 
   // Called when the command is initially scheduled.
