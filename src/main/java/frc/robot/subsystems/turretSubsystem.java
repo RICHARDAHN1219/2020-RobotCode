815c08825acf8b9695437d41de0fba6fecd9515c
Dylan Reiner <dreiner@live.com>
2020-02-24 11:59:29 -0800

    i betcha i just fixed the turret

--- src/main/java/frc/robot/subsystems/turretSubsystem.java
+++ src/main/java/frc/robot/subsystems/turretSubsystem.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.turretConstants;
 import static frc.robot.Constants.turretConstants.kSoftMaxTurretAngle;
 import static frc.robot.Constants.turretConstants.kSoftMinTurretAngle;
 import static frc.robot.Constants.turretConstants.kDegreesPerTick;
@@ -34,11 +35,11 @@ public class turretSubsystem extends SubsystemBase {
 
     // TODO: fix rotational direction and sensor phase
     turretDrive.setInverted(true); // CCW is positive direction
-    turretDrive.setSensorPhase(false);
+    turretDrive.setSensorPhase(true);
 
     // set soft limits
-    turretDrive.configForwardSoftLimitThreshold(-6152);
-    turretDrive.configReverseSoftLimitThreshold(4864);
+    turretDrive.configForwardSoftLimitThreshold(-6152, kTimeout);
+    turretDrive.configReverseSoftLimitThreshold(4864, kTimeout);
     turretDrive.configForwardSoftLimitEnable(true);
     turretDrive.configReverseSoftLimitEnable(true);
 
